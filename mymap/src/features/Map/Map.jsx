0b68ee1baf9dd2fc806d2b7becae933828db5394
Sona Khachatryan <sona.khachatryannn@gmail.com>
2024-04-12 18:00:35 +0400

    added photos

--- mymap/src/features/Map/Map.jsx
+++ mymap/src/features/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {getFeatures, getName} from "../../thunks/getData";
-import {Alert, Grid, Paper, styled, Typography} from "@mui/material";
+import {Alert, Button, Card, CardActions, CardContent, CardMedia, Grid, Paper, styled, Typography} from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : 'rgb(164, 138, 125)',
@@ -57,11 +57,26 @@ function Map(props) {
                 </Typography>
                 <Grid container spacing={2}>
                     {features.map(place => <Grid item xs={6} md={3}>
-                        <Item>
-                            {place.properties.Name} <br/>
-                            {`Longitude:${place.geometry.coordinates[0]}`} <br/>
-                            {`Latitude:${place.geometry.coordinates[1]}`}
-                        </Item>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                sx={{ height: 140 }}
+                                image={place.properties.gx_media_links}
+                                title="location"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {place.properties.Name}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {`Longitude:${place.geometry.coordinates[0]}`} <br/>
+                                    {`Latitude:${place.geometry.coordinates[1]}`}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
                     </Grid>)}
                 </Grid>
             </div>
